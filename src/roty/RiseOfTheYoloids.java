9f9ac32f6a6f3405aec81b0508ad6042a516fff5
Felix Sjöö <fiss.01@gmail.com>
2014-02-03 18:42:24 +0100

    2nd Try

--- src/roty/RiseOfTheYoloids.java
+++ src/roty/RiseOfTheYoloids.java
@@ -1,12 +1,10 @@
 package roty;
 
-public class RiseOfTheYoloids {
+public class RiseOfTheYoloids 
+{
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
 
 	}
 
