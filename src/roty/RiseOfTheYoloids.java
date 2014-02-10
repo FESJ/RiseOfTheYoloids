4f40013f360fb54ed36868aefaa719d32fc74964
Dennis <Dennis@172.16.6.162>
2014-02-10 13:06:49 +0100

    n

--- src/roty/RiseOfTheYoloids.java
+++ src/roty/RiseOfTheYoloids.java
@@ -1,11 +1,38 @@
+// Work in Progress: NO
+
 package roty;
 
-public class RiseOfTheYoloids 
+import java.awt.event.*;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class RiseOfTheYoloids extends JFrame implements KeyListener 
 {
 
-	public static void main(String[] args) 
-	{
-
+	// Constructor
+	public RiseOfTheYoloids() 
+	{		
+		super("Rise of the Yoloids");
+		
+		this.addKeyListener(this);
+		
+		setSize(768, 480);
+		setVisible(true);
+	
 	}
-
+		
+	public void keyTyped(KeyEvent e) {}
+	public void keyPressed(KeyEvent e) {
+		JOptionPane.showMessageDialog(null, "Tastencode: "+e.getKeyCode());
+	}
+	public void keyReleased(KeyEvent e) {}
+	/**
+	 * vfvfdv
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new RiseOfTheYoloids();
+	}
 }
+
