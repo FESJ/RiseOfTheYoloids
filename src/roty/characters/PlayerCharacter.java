7bb05cdd8c15ed3a99f26fe5eaa6a7d3ba5b3dac
Dennis <dennis@deidert.net>
2014-02-10 14:41:38 +0100

    Actions, fehlerhaft

--- src/roty/characters/PlayerCharacter.java
+++ src/roty/characters/PlayerCharacter.java
@@ -3,7 +3,6 @@ package roty.characters;
 import java.awt.Graphics;
 import java.awt.event.*;
 
-
 import javax.swing.*;
 
 public class PlayerCharacter extends JPanel implements ActionListener
@@ -12,10 +11,15 @@ public class PlayerCharacter extends JPanel implements ActionListener
 	private static final long serialVersionUID = -7499253372209406900L;
 	private int[] position = {100, 100};
 
-	public PlayerCharacter(){}
+	public PlayerCharacter()
+	{
+		this.getInputMap().put(KeyStroke.getKeyStroke("DOWN"), "down");
+	 	this.getActionMap().put("down", );
+	}
 
 	public void actionPerformed(ActionEvent e) 
 	{
+		JOptionPane.showMessageDialog(null, "Tastencode:Kartoffel")
 		repaint();
 	}
 	
