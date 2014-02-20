b70df868f58697bd427cbe883f0c9a135eef4b0b
Felix Sjöö <fiss.01@gmail.com>
2014-02-20 14:53:24 +0100

    Added worldsize to RotyTools

--- src/roty/world/misc/RotyTools.java
+++ src/roty/world/misc/RotyTools.java
@@ -4,14 +4,26 @@ import java.awt.*;
 
 public class RotyTools 
 {
+	private static Dimension worldsize;
+	
 	public RotyTools(){}
 	
-	public static boolean isOutOfBounds(Point pos, Dimension dim, Dimension boundaries)
+	public static void setWorldSize(Dimension _worldsize)
+	{
+		worldsize = _worldsize;
+	}
+	
+	public static Dimension getWorldSize()
+	{
+		return worldsize;
+	}
+	
+	public static boolean isOutOfBounds(Point pos, Dimension dim)
 	{	
 		if(pos.x >= 0 
 		&& pos.y >= 0 
-		&& pos.x + dim.width <= boundaries.width 
-		&& pos.y + dim.height <= boundaries.height)
+		&& pos.x + dim.width <= worldsize.width 
+		&& pos.y + dim.height <= worldsize.height)
 						return false;
 		
 		return true;
