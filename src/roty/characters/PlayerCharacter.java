faf3effe52f78dac49cbc7aa4ec2d5855617b998
Felix Sjöö <fiss.01@gmail.com>
2014-03-20 14:01:02 +0100

    Player Image shows! Yay!

--- src/roty/characters/PlayerCharacter.java
+++ src/roty/characters/PlayerCharacter.java
@@ -45,7 +45,8 @@ public class PlayerCharacter extends JLabel implements ActionListener
 	{
 		try
 		{
-			java.net.URL imgURL = PlayerCharacter.class.getResource("images/guys_01_"+direction+".png");
+			java.net.URL imgURL = PlayerCharacter.class.getResource("../images/guy_01_"+direction+".png");
+			System.out.println(imgURL.getPath());
 			
 			if (imgURL != null)
 	        {
